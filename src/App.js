89a5424db96b267350cd6a6c7069e2181126dcd2
Miley Gao <miley19901018@gmail.com>
2018-05-08 15:59:58 -0700

    cos upload file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import cos from './utils/cos'
 
 class App extends Component {
+  onFileChange = (e)=>{
+    if (e.target.files && e.target.files[0]) {
+      cos.uploadFile(e.target.files[0]).then(console.log).catch(console.error)
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +19,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src="https://test-1256370191.cos.ap-guangzhou.myqcloud.com/bd_logo1.png" alt="baidu logo" />
+        <input type="file" onChange={this.onFileChange}/>
       </div>
     );
   }
--- src/utils/cos.js
+++ src/utils/cos.js
@@ -1,4 +1,5 @@
 import COS from 'cos-js-sdk-v5'
+import axios from 'axios'
 const Bucket = 'test-1256370191'
 const Region = 'ap-guangzhou';
 
@@ -6,29 +7,34 @@ const Region = 'ap-guangzhou';
 const cos = new COS({
   getAuthorization: function (options, callback) {
     // 异步获取签名
-    $.get('../server/auth.php', {
-      method: (options.Method || 'get').toLowerCase(),
-      pathname: '/' + (options.Key || '')
-    }, function (authorization) {
-      callback(authorization);
-    }, 'text');
+    const method = (options.Method || 'get').toLowerCase()
+    const pathname = '/' + (options.Key || '')
+    axios({
+      url:'http://localhost:9001/api/cos/auth',
+      params:{
+        method: method,
+        pathname: pathname,
+      },
+      responseType: 'text',
+    }).then(response=>callback(response.data))
   }
 });
 
-// 监听选文件
-document.getElementById('file-selector').onchange = function () {
-
-  var file = this.files[0];
-  if (!file) return;
-
-  // 分片上传文件
-  cos.sliceUploadFile({
-    Bucket: Bucket,
-    Region: Region,
-    Key: file.name,
-    Body: file,
-  }, function (err, data) {
-    console.log(err, data);
-  });
-
-};
+export default {
+  async uploadFile(file){
+    return new Promise((resolve, reject)=>{
+      cos.sliceUploadFile({
+        Bucket: Bucket,
+        Region: Region,
+        Key: file.name,
+        Body: file,
+      }, function (err, data) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      });
+    })
+  }
+}
