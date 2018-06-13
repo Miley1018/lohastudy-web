1091c7e140b2d084dd8865a7b0989a5695326e89
Miley Gao <miley19901018@gmail.com>
2018-06-13 13:38:12 -0700

    fix deploy; build locally

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -t kingo@lohastudy.com 'cd lehuo/web;git checkout package-lock.json;git pull && npm i && node ./node_modules/webpack/bin/webpack.js && cp -r style dist/'&& echo success
+# ssh -t kingo@lohastudy.com 'cd lehuo/web;git checkout package-lock.json;git pull && npm i && node ./node_modules/webpack/bin/webpack.js && cp -r style dist/'&& echo success
+node ./node_modules/webpack/bin/webpack.js && scp -r dist/* style kingo@lohastudy.com:~/lehuo/web/dist/ && echo success
