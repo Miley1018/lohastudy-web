8319347a58653810b7e443906a3aab3e86aab42b
Miley Gao <miley19901018@gmail.com>
2018-06-20 17:59:03 -0700

    fix

--- src/actions/request.js
+++ src/actions/request.js
@@ -2,31 +2,38 @@ import axios from 'axios';
 
 let host = 'https://lohastudy.com/prod/api'
 export function requestWithToken(url, method, content) {
-    return axios(
-        {
-            url: host + url,
-            method: method,
-            headers: {
-                Authorization: 'Bearer '+localStorage.getItem('token')},
-            data: content
-        }
-    ).then().catch(error => {
-        if (error.response.status === 401) {
-            location.href = '/signin'
-          return
-        }
-        alert(error.response.data.errors.message)
-    })
+  const config = {
+    url: host + url,
+    method: method,
+    headers: {
+      Authorization: 'Bearer '+localStorage.getItem('token')},
+  }
+  if (method.toLowerCase() == 'get') {
+    config.params = content
+  } else {
+    config.data = content
+  }
+  return axios(config).then().catch(error => {
+    if (error.response.status === 401) {
+      location.href = '/signin'
+      return
+    }
+    alert(error.response.data.errors.message)
+  })
 }
 
 export function request(url, method, content) {
-      return axios(
-        {
-          url: host + url,
-          method: method,
-          data: content
-        }
-      ).then().catch(error => {
-        console.log(error)
+  const config = {
+    url: host + url,
+    method: method,
+  }
+  if (method.toLowerCase() == 'get') {
+    config.params = content
+  } else {
+    config.data = content
+  }
+  return axios(config).then().catch(error => {
+    console.log(error)
   })
 }
+
